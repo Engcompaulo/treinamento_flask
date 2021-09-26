3a7e3c04282ba3cc6e3e52418438107e94a29102
engcompaulo <engcompaulotic@gmail.com>
2021-09-26 08:17:55 -0300

    Fenda

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec
 
 server = Flask(__name__)
+spec = FlaskPydanticSpec('flask', title='Treinamento do Paulo')
+spec.register(server)
 
 @server.get('/cadastros')
 def pegar_cadastros():
