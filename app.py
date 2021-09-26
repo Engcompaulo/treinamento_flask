3dc51683a2e4b016484e4f88e7bcc65563aa8e11
engcompaulo <engcompaulotic@gmail.com>
2021-09-26 16:43:40 -0300

    Adição do banco

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
 from pydantic import BaseModel
+from tinydb import TinyDB, Query
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec('flask', title='Treinamento do Paulo')
 spec.register(server)
+database = TinyDB('database.json')
 
 class Pessoa(BaseModel):
     id: int
@@ -12,15 +14,17 @@ class Pessoa(BaseModel):
     idade: int
 
 @server.get('/cadastros')
-@spec.validate(resp=Response(HTTP_200=Pessoa))
 def pegar_cadastros():
-    return "Felipe Luiz"
+    """Retorna todas os cadastros da base de dados. """
+    return jsonify(database.all())
+
 
 @server.post('/cadastros')
 @spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
 def inserir_cadastro():
     """Insere um novo cadastro no banco de dados."""
     body = request.context.body.dict()
+    database.insert(body)
     return body
     ...
 
